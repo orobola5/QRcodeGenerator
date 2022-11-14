82c709364d311577d5059fd9eec416e25d4e2126
Love Olanrewaju <love.olanrewaju@cicod.com>
2022-11-14 02:49:51 +0100

    initial commit

---
+++ src/main/java/com/loveProject/springbootQRcodegeneratorapp/SpringBootQrCodeGeneratorAppApplication.java
@@ -0,0 +1,13 @@
+package com.loveProject.springbootQRcodegeneratorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGeneratorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrCodeGeneratorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loveProject/springbootQRcodegeneratorapp/SpringBootQrCodeGeneratorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loveProject.springbootQRcodegeneratorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGeneratorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
